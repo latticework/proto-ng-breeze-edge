81e6c8ccc33ae7694fda37d4ca4e157ab37e2d94
gulshan-pittie <gulshan.pittie@gmail.com>
2013-08-13 16:41:07 -0400

    added server.ts and updated init.ps1

--- init.ps1
+++ init.ps1
@@ -9,3 +9,6 @@ Write-Host -ForegroundColor DarkGreen "Complied 'build.config.ts' to 'build.conf
 
 tsc --module commonjs .\Gruntfile.ts 
 Write-Host -ForegroundColor DarkGreen "Complied 'Gruntfile.ts' to 'Gruntfile.js'"
+
+tsc --module commonjs .\src\server\server.ts
+Write-Host -ForegroundColor DarkGreen "Complied 'Server.ts' to 'Server.js'"
